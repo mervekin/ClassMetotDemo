9d2ac4d4fd38ab36f6b456e4b5d36a580fcf5fa4
Merve Bozkurt <bbozkurtmerve@gmail.com>
2021-01-20 15:49:24 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusEkle(Musteri musteri)
+        {
+            Console.WriteLine("Yeni müşteri eklendi " + musteri.Ad + " " + musteri.Soyad);
+            Console.WriteLine("----------------------------------------");
+
+        }
+        public void MusSil(Musteri musteri)
+        {  
+            Console.WriteLine("Müşteri Idsi " + musteri.Id + " olan "+ musteri.Ad +" "+musteri.Soyad+" silindi.");
+            Console.WriteLine("----------------------------------------");
+        }
+
+        public void MusListele(Musteri[] musteriler)
+        {
+            Console.WriteLine("----------------Müşteri Listesi--------------");
+            foreach (Musteri musteri in musteriler)
+            {
+                
+                Console.WriteLine("Adı:" + musteri.Ad);
+                Console.WriteLine("Soyad:" + musteri.Soyad);
+                Console.WriteLine("Adress:" + musteri.Adress);
+                Console.WriteLine("----------------------------------------");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Luke";
+            musteri1.Soyad = "Skywalker";
+            musteri1.Adress = "Tatooine";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Leia";
+            musteri2.Soyad = "Organa Skywalker";
+            musteri2.Adress = "Alderaan";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Darth";
+            musteri3.Soyad = "Vader";
+            musteri3.Adress= "Tatooine";
+
+            Musteri[] musteriler = new Musteri[] {musteri1,musteri2,musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusListele(musteriler);
+            musteriManager.MusEkle(musteri3);
+            musteriManager.MusSil(musteri2);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
